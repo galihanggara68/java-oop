7839038acbec1eae433bf0b4a73d4e36fbe54ad2
Galih Anggara <galihanggara68@gmail.com>
2020-03-19 11:03:38 +0700

    Create FirstApp.java

---
+++ FirstApp.java
@@ -0,0 +1,27 @@
+package first;
+
+public class FirstApp {
+
+	public static void main(String[] args) {
+		// Dunia Virtual Pertama
+		// Merealisasikan rancangan/class Person
+		Person person = new Person();
+		
+		person.setTinggiBadan(160);
+		person.setWarnaKulit("Sawo Matang");
+		person.setWarnaRambut("Hitam");
+		person.setSedangLapar(true);
+	
+		System.out.println(person.getTinggiBadan());
+		System.out.println(person.getWarnaKulit());
+		System.out.println(person.getWarnaRambut());
+		
+		System.out.println("Sebelum Makan");
+		System.out.println("Sedang Lapar : " + person.isSedangLapar());
+		
+		person.makan();
+		
+		System.out.println("Setelah Makan");
+		System.out.println("Sedang Lapar : " + person.isSedangLapar());
+	}
+}
